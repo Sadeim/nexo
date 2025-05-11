38b2d856ffa0076b6e897ee16abb48c2175ca644
Ahmed Samna <Ahmed31916@gmail.com>
2025-05-11 10:13:01 +0300

    ed

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -24,6 +24,7 @@ class SettingSeeder extends Seeder
             'twitter'      => 'https://twitter.com/company/example',
             'address'      => '993 Renner Burg, West Rond, MT 94251-030',
             'web_address'  => 'https://example.com',
+            'site_description'  => 'Professionally develop long-term performance based architectures metrics rather than',
             'map_embed'  => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.2528000654!2d-74.14448744699546!3d40.69763123333061!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1733550855459!5m2!1sen!2sbd',
         ];
 
