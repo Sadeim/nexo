0ef719a616ef32d9c88f793bed59a04e665eca27
Hosni H. Abu Shbak <66937367+hosniabushbak@users.noreply.github.com>
2025-05-20 18:59:54 -0500

    a

--- resources/views/frontend/about_us.blade.php
+++ resources/views/frontend/about_us.blade.php
@@ -324,7 +324,7 @@
                             <!-- Why Choose Image Start -->
                             <div class="why-choose-image">
                                 <figure class="image-anime">
-                                    <img src="{{asset('frontend_assets/images/why.png')}}" alt="">
+                                    <img src="{{asset('frontend_assets/images/why.jpg')}}" alt="">
                                 </figure>
                             </div>
                             <!-- Why Choose Image End -->
