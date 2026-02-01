12d08b237ef1b9ce02df447e14f701293fe9a2c0
Haneen Hanounaa <68811687+haneenhanounaa@users.noreply.github.com>
2026-02-01 11:37:31 +0200

    05

--- app/Traits/SaveImageTrait.php
+++ app/Traits/SaveImageTrait.php
@@ -1,19 +1,20 @@
 <?php
 
 namespace App\Traits;
+
 use Intervention\Image\Facades\Image;
 
 trait SaveImageTrait
 {
-    protected function uploadImage($file, $path = '')
-    {
-        $file_exe = $file->getClientOriginalExtension();
-        $new_name = uniqid() . '.' . $file_exe;
-        $directory = 'uploads' . '/' . 'store' . '/' . $path;
-        $destienation = public_path($directory);
-        $file->move($destienation, $new_name);
-        return '/' . $directory . '/' . $new_name;
-    }
+    // protected function uploadImage($file, $path = '')
+    // {
+    //     $file_exe = $file->getClientOriginalExtension();
+    //     $new_name = uniqid() . '.' . $file_exe;
+    //     $directory = 'uploads' . '/' . 'store' . '/' . $path;
+    //     $destienation = public_path($directory);
+    //     $file->move($destienation, $new_name);
+    //     return '/' . $directory . '/' . $new_name;
+    // }
 
     // protected function uploadImage($file, $path = '')
     // {
@@ -36,4 +37,21 @@ trait SaveImageTrait
 
     //     return '/' . $directory . '/' . $new_name;
     // }
+
+    protected function uploadImage($file, $path = '')
+    {
+        $file_exe = $file->getClientOriginalExtension();
+        $new_name = uniqid() . '.' . $file_exe;
+        $directory = 'uploads' . '/' . 'store' . '/' . $path;
+        $destination = public_path($directory);
+
+        // إنشاء المجلد تلقائياً إذا لم يكن موجوداً
+        if (!file_exists($destination)) {
+            mkdir($destination, 0777, true);
+            chmod($destination, 0777); // تأكيد الصلاحيات
+        }
+
+        $file->move($destination, $new_name);
+        return '/' . $directory . '/' . $new_name;
+    }
 }
