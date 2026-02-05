8ca990632fc31410698baf7a179cb272ac1060af
Haneen Hanounaa <68811687+haneenhanounaa@users.noreply.github.com>
2026-02-05 14:01:32 +0200

    25

--- app/Traits/SaveImageTrait.php
+++ app/Traits/SaveImageTrait.php
@@ -57,6 +57,40 @@ trait SaveImageTrait
     //     return '/' . $directory . '/' . $new_name;
     // }
 
+    // protected function uploadImage($file, $path = '')
+    // {
+    //     // تسجيل حجم الملف الأصلي
+    //     Log::info('Original file size: ' . $file->getSize() . ' bytes');
+
+    //     $file_exe = $file->getClientOriginalExtension();
+    //     $new_name = uniqid() . '.' . $file_exe;
+    //     $directory = 'uploads' . '/' . 'store' . '/' . $path;
+    //     $destination = public_path($directory);
+
+    //     // إنشاء المجلد تلقائياً إذا لم يكن موجوداً
+    //     if (!file_exists($destination)) {
+    //         mkdir($destination, 0755, true);
+    //     }
+
+    //     // قراءة الصورة بدون ضغط
+    //     $image = Image::make($file->getRealPath());
+
+    //     // حفظ الصورة بجودة 100% مباشرة
+    //     $fullPath = $destination . '/' . $new_name;
+    //     $image->save($fullPath, 100);
+
+    //     // تسجيل حجم الملف بعد الرفع
+    //     if (file_exists($fullPath)) {
+    //         $uploadedSize = filesize($fullPath);
+    //         Log::info('Uploaded file size: ' . $uploadedSize . ' bytes');
+
+    //         // حساب نسبة الضغط
+    //         $compressionRatio = round((1 - ($uploadedSize / $file->getSize())) * 100, 2);
+    //         Log::info('Compression ratio: ' . $compressionRatio . '%');
+    //     }
+
+    //     return '/' . $directory . '/' . $new_name;
+    // }
     protected function uploadImage($file, $path = '')
     {
         // تسجيل حجم الملف الأصلي
@@ -72,12 +106,10 @@ trait SaveImageTrait
             mkdir($destination, 0755, true);
         }
 
-        // قراءة الصورة بدون ضغط
-        $image = Image::make($file->getRealPath());
-
-        // حفظ الصورة بجودة 100% مباشرة
         $fullPath = $destination . '/' . $new_name;
-        $image->save($fullPath, 100);
+
+        // نسخ الملف مباشرة بدون أي معالجة (يحافظ على الأبعاد والجودة 100%)
+        copy($file->getRealPath(), $fullPath);
 
         // تسجيل حجم الملف بعد الرفع
         if (file_exists($fullPath)) {
@@ -87,6 +119,14 @@ trait SaveImageTrait
             // حساب نسبة الضغط
             $compressionRatio = round((1 - ($uploadedSize / $file->getSize())) * 100, 2);
             Log::info('Compression ratio: ' . $compressionRatio . '%');
+
+            // تسجيل أبعاد الصورة
+            if (function_exists('getimagesize')) {
+                $imageInfo = getimagesize($fullPath);
+                if ($imageInfo) {
+                    Log::info('Image dimensions: ' . $imageInfo[0] . ' x ' . $imageInfo[1] . ' pixels');
+                }
+            }
         }
 
         return '/' . $directory . '/' . $new_name;
